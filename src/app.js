c6dac4efe20252a6d2e6d828c1c61e6a5f3510e3
elzagwong <44502660+elzagwong@users.noreply.github.com>
2023-10-27 22:31:38 +0200

    created-datetime-function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function formatDate(timestamp) {
+
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes<10) {
+    minutes = `0${minutes}`
+  };
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+
+
+}
+
 
 function showWeather(response) {
   let temperatureElement = document.querySelector("#main-current-temp");
   let temperature = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#search-input-city");
   cityElement.innerHTML = response.data.name;
-  temperatureElement.innerHTML = `${temp}°C `;
+  temperatureElement.innerHTML = `${temperature}°C `;
   
   let currentHumidity = document.querySelector("#humidity");
   let humid = Math.round(response.data.main.humidity);
@@ -21,14 +38,16 @@ function showWeather(response) {
   let currentMinTemp = document.querySelector("#current-min");
   let tempMin = Math.round(response.data.main.temp_min);
   currentMinTemp.innerHTML = `${tempMin}°C `;
+
+  let dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   
 }
 
 
-let search = document.querySelector("#search-form");
-search.addEventListener("submit", searchCity)
+let city = "Choluteca"
 let apiKey = "f16cae8dd5b86ff5840e0c571a06e631";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiURL).then(showWeather)
+axios.get(apiUrl).then(showWeather)
 
